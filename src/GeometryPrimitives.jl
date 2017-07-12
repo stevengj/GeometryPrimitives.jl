646368bdcf632735bf02b6fb94e2b013b43017cd
Wonseok Shin <wsshin97@gmail.com>
2017-07-12 12:55:15 -0400

    Handle unresolved conflicts in the previous cherry-pick

--- src/GeometryPrimitives.jl
+++ src/GeometryPrimitives.jl
@@ -2,13 +2,8 @@ module GeometryPrimitives
 
 using Compat, StaticArrays
 
-<<<<<<< HEAD
-@compat abstract type Shape{N,D} end # a solid geometric shape in N dimensions
-Base.ndims{N}(o::Shape{N}) = N
-=======
 abstract type Shape{N} end # a solid geometric shape in N dimensions
 Base.ndims(o::Shape{N}) where {N} = N
->>>>>>> 0f8dd2b... Drop support for Julia 0.5, and use Julia 0.6 syntax (#7)
 
 export Shape, normal, bounds
 
