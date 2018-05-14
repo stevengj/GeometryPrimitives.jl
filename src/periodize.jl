a73c2bdca4c594743f5547b44e7094bb49b650cb
Wonseok Shin <wsshin97@gmail.com>
2018-05-14 17:41:35 -0400

    Include lattice vectors on boundaries; add comments; improve test

--- src/periodize.jl
+++ src/periodize.jl
@@ -2,33 +2,48 @@ export periodize
 
 periodize(o::Shape{N}, A::AbstractMatrix, ∆range::Shape{N}) where {N} = periodize(o, SMatrix{N,N}(A), ∆range)
 
+
 function periodize(o::S,  # shape to periodize
-                   A::SMatrix{N,N,<:Real,L},  # columns are primitive vectors of lattice
-                   ∆range::Shape{N,L}  # range of shift
+                   A::SMatrix{N,N,<:Real,L},  # columns: primitive vectors of Bravais lattice
+                   ∆range::Shape{N,L}  # range of translation vectors; boundaries included
                   ) where {N,L,S<:Shape{N,L}}
-    ∆bound = bounds(∆range)  # bounds of shift range
-    nmax_fl = SVector(ntuple(k->-Inf,Val{N}))
-    nmin_fl = SVector(ntuple(k->Inf,Val{N}))
-    cr = CartesianRange(ntuple(k->2, Val{N}))  # (2,2,2) for N = 3
-    for ci = cr
-        cnr = map((∆b1,∆b2,i)-> i==1 ? ∆b1 : ∆b2, ∆bound[1], ∆bound[2], SVector(ci.I))  # SVector: corner of ∆bound (ci.I[k] = 1 or 2)
+    # R = n₁a₁ + n₂a₂ + n₃a₃ is a translation vector for A = [a₁ a₂ a₃].  Find the ranges of
+    # n₁, n₂, n₃ to test the inclusion of R in the Shape ∆range.
+    #
+    # The minimum and maximum nᵢ's to examine can be found by finding nᵢ's at the corners of
+    # the bounding box of ∆range.  This can be easily seen by unitary transformation of aᵢ's
+    # into the Cartesian directions.  The transformation transforms the lattice planes into
+    # the x-, y-, z-normal planes, and the bounding box into a parallelpiped.  The nᵢ's at
+    # the corners of the original bounding box corresponds to the Cartesian indices of the
+    # corners of the parallelpiped after transformation, and the minimum and maximum nᵢ's
+    # are the Cartesian indices of the corners of the bounding box of this post-transform
+    # parallelpiped.  Because the post-transform parallelpiped is enclosed by its bounding
+    # box, all the lattice points within the post-transform parallelpiped are also within
+    # the bounding box of the parallelpiped, whose corner indices are the minimum and
+    # maximum nᵢ's.
+    ∆bound = bounds(∆range)  # (SVector{3}, SVector{3}): bounds of translation range
+    nmax_fl = SVector(ntuple(k->-Inf, Val{N}))  # [-Inf, -Inf, -Inf] for N = 3
+    nmin_fl = SVector(ntuple(k->Inf, Val{N}))  # [Inf, Inf, Inf] for N = 3
+    rcart = CartesianRange(ntuple(k->2, Val{N}))  # (2,2,2) for N = 3
+    for icart = rcart  # icart: CartesianIndex (see, e.g., https://julialang.org/blog/2016/02/iteration)
+        cnr = map((∆b1,∆b2,i)-> i==1 ? ∆b1 : ∆b2, ∆bound[1], ∆bound[2], SVector(icart.I))  # SVector: corner of ∆bound (note icart.I[k] = 1 or 2)
         n_fl = A \ cnr  # SVector: A * n_fl = corner
         nmax_fl = max.(nmax_fl, n_fl)  # SVector
         nmin_fl = min.(nmin_fl, n_fl)  # SVector
     end
 
-    r = 1.0 - Base.rtoldefault(Float64)  # scale factor to multiply to exclude points on boundary
-    # Below, use `r.*` for fusion after StaticArrays Issue 390 is resolved.
-    nmax = floor.(Int, r * nmax_fl)  # SVector
-    nmin = ceil.(Int, r * nmin_fl)  # SVector
+    # Find the integral nᵢ's.
+    nmax = ceil.(Int, nmax_fl)  # SVector
+    nmin = floor.(Int, nmin_fl)  # SVector
 
+    # For nᵢ's within the range calculated above, check if R = n₁a₁ + n₂a₂ + n₃a₃ is within
+    # the Shape ∆range.  If it is, then create a shape transformed by R.
     nshape = prod((nmax.-nmin).+1)  # tentative number of shapes
-    shp_array = Vector{S}(nshape)  # preallocation (will be trimmed later)
-
+    shp_array = Vector{S}(nshape)  # preallocation (will be truncated later)
     ishape = 0
     nrange = map((n1,n2)->n1:n2, nmin.data, nmax.data)  # e.g., (1:10, 1:10, 1:10) for N = 3
-    for n = CartesianRange(nrange)
-		∆ = A * SVector(n.I)  # lattice vector
+    for n = CartesianRange(nrange)  # n: CartesianIndex
+		∆ = A * SVector(n.I)  # lattice vector R
 		if ∆ ∈ ∆range
 			shape = translate(o, ∆)
 			ishape += 1
--- test/runtests.jl
+++ test/runtests.jl
@@ -286,36 +286,55 @@ end
 
         @testset "periodize" begin
             # Square lattice
+            using GeometryPrimitives
             c = Cylinder([0,0,0], 1, [0,0,1], 5)
-            b = Box([0,0,0], [10,10,5])
+            ∆range = Box([0,0,0], [10,10,5])
             A = [1 0 0; 0 1 0; 0 0 5]'
-            c_array = periodize(c, A, b)
-            @test length(c_array) == (11-2)^2
-
-            # Hexagonal lattice
-            using PyPlot
-            c = Cylinder([0,0,0], 1/4, [0,0,1], 1)
-            b = Box([0,0,0], [12,4*√3,1])
-            A = [1 0 0; 0.5 √3/2 0; 0 0 1]'
-            c_array = periodize(c, A, b)
-
-            bnd = bounds(b)
-            x = linspace(bnd[1][1], bnd[2][1], 120*5)
-            y = linspace(bnd[1][2], bnd[2][2], 70*5)
-            Nx, Ny = length(x), length(y)
-            X = repeat(x, outer=(1,Ny))
-            Y = repeat(y.', outer=(Nx,1))
-            V = zeros(Bool, Nx, Ny)
+            c_array = periodize(c, A, ∆range)
+            # @test length(c_array) == 11^2  # test if correct number of cylinders are generated
 
             kd = KDTree(c_array)
-            for j = 1:Ny, i = 1:Nx
-                p = [x[i], y[j], 0.0]
+            result = true
+            for py = -5.0:5.0, px = -5.0:5.0
+                p = [px, py, 0.0]
                 s = findin(p, kd)
-                V[i,j] = !isnull(s)
+                result &= !isnull(s)  # test if any lattice point within ∆range is included in a cylinder
             end
+            @test result
 
-            axes(aspect="equal")
-            pcolor(X, Y, V, cmap="gray_r")
+            # Execute the following lines to test a hexagonal lattice visually.
+            # c = Cylinder([0,0,0], 1/4, [0,0,1], 1)
+            # ∆range = Box([0,0,0], [8,2*√3,1])
+            # A = [1 0 0; 0.5 √3/2 0; 0 0 1]'
+            # c_array = periodize(c, A, ∆range)
+            #
+            # b = Box([0,0,0], [12,4*√3,1])
+            # bnd = bounds(b)
+            # x = linspace(bnd[1][1], bnd[2][1], 120*5)
+            # y = linspace(bnd[1][2], bnd[2][2], 70*5)
+            # Nx, Ny = length(x), length(y)
+            # X = repeat(x, outer=(1,Ny))
+            # Y = repeat(y.', outer=(Nx,1))
+            # V = zeros(Bool, Nx, Ny)
+            #
+            # kd = KDTree(c_array)
+            # for j = 1:Ny, i = 1:Nx
+            #     p = [x[i], y[j], 0.0]
+            #     s = findin(p, kd)
+            #     V[i,j] = !isnull(s)
+            # end
+            #
+            # using PyPlot
+            # clf()
+            # axes(aspect="equal")
+            # pcolor(X, Y, V, cmap="gray_r")
+            #
+            # using PyCall
+            # @pyimport matplotlib.patches as patch
+            # b∆range = bounds(∆range)
+            # cnr = b∆range[1][1:2]
+            # w, h = -((-)(b∆range...)[1:2])
+            # gca()[:add_artist](patch.Rectangle(cnr, w, h, fill=false, edgecolor="r", linestyle="--"))
         end
     end
 
