3146db17d7fafda795937182f2145d3ccac32664
Steven G. Johnson <stevenj@mit.edu>
2023-01-21 10:56:48 -0600

    remove AbbreviatedTypes usage (#41)
    * remove AbbreviatedTypes usage
    
    * rm abbrev in comment

--- src/GeometryPrimitives.jl
+++ src/GeometryPrimitives.jl
@@ -1,5 +1,6 @@
 module GeometryPrimitives
-using AbbreviatedTypes; @define_types_with(Float64)
+
+using StaticArrays
 using LinearAlgebra
 using Statistics: mean
 using Makie
@@ -14,27 +15,31 @@ const Shape3 = Shape{3,9}
 
 Base.ndims(::Shape{N}) where {N} = N
 
+# relative tolerance * x (assumed ≥ 0) for approximate comparisons,
+# defined to square root of machine precision like Base.rtoldefault
+rtol(x::AbstractFloat) = sqrt(eps(typeof(x))) * x
+
 # The following functions return Any due to the limitations of Julia's dispatch system.
 # Therefore, always call them with return type assertions.  See
 # https://discourse.julialang.org/t/extending-base-in-type-stably/5341/12
 # https://github.com/JuliaLang/julia/issues/23210
-level(x::AbsVecReal, s::Shape{N}) where {N} = level(SVec{N}(x), s)
-Base.in(x::AbsVecReal, s::Shape{N}) where {N} = level(x,s) ≥ 0
-surfpt_nearby(x::AbsVecReal, s::Shape{N}) where {N} = surfpt_nearby(SVec{N}(x), s)
-normal(x::AbsVecReal, s::Shape) = surfpt_nearby(x, s)[2]  # outward direction even for x inside s
-translate(s::Shape{N}, ∆::AbsVecReal) where {N} = translate(s, SVec{N}(∆))
-translate(s::Shape{N}, ∆::SReal{N}) where {N} = (s2 = deepcopy(s); s2.c += ∆; s2)  # default implementation
+level(x::AbstractVector{<:Real}, s::Shape{N}) where {N} = level(SVector{N}(x), s)
+Base.in(x::AbstractVector{<:Real}, s::Shape{N}) where {N} = level(x,s) ≥ 0
+surfpt_nearby(x::AbstractVector{<:Real}, s::Shape{N}) where {N} = surfpt_nearby(SVector{N}(x), s)
+normal(x::AbstractVector{<:Real}, s::Shape) = surfpt_nearby(x, s)[2]  # outward direction even for x inside s
+translate(s::Shape{N}, ∆::AbstractVector{<:Real}) where {N} = translate(s, SVector{N}(∆))
+translate(s::Shape{N}, ∆::SVector{N,<:Real}) where {N} = (s2 = deepcopy(s); s2.c += ∆; s2)  # default implementation
 
-function orthoaxes(n::SReal{3})
-    u_temp = abs(n[3]) < abs(n[1]) ? SVec(0,0,1) : SVec(1,0,0)
+function orthoaxes(n::SVector{3,<:Real})
+    u_temp = abs(n[3]) < abs(n[1]) ? SVector(0,0,1) : SVector(1,0,0)
     v = normalize(n × u_temp)
     u = v × n
 
     return u, v
 end
 
-function orthoaxes(n::SReal{N}) where {N}
-    u_temp = abs(n[3]) < abs(n[1]) ? SVec(0,0,1) : SVec(1,0,0)
+function orthoaxes(n::SVector{N,<:Real}) where {N}
+    u_temp = abs(n[3]) < abs(n[1]) ? SVector(0,0,1) : SVector(1,0,0)
     v = normalize(n × u_temp)
     u = v × n
 
